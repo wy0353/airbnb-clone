f73ee654362543035f862922ba71b76b04e553d0
ethan <wy0353@gmail.com>
2021-01-22 16:28:46 +0900

    Created reservation model & admin

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models as reservation_models
 
-# Register your models here.
+
+@admin.register(reservation_models.Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+
+    """ Reservation Admin Definition """
+
+    pass
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Reservation(core_models.DefaultModel):
+
+    """ Reservation Model Definition """
+
+    STATUS_PENDING = "pending"
+    STATUS_CONFIRMED = "confirmed"
+    STATUS_CANCELED = "canceled"
+    STATUS_CHOICES = (
+        (STATUS_PENDING, "Pending"),
+        (STATUS_CONFIRMED, "Confirmed"),
+        (STATUS_CANCELED, "Canceled"),
+    )
+
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING)
+    check_in = models.DateField(null=True, blank=True)
+    check_out = models.DateField(null=True, blank=True)
+    guest = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="reservations")
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE, related_name="reservations")
+
+    def __str__(self):
+        return f"{self.room.name} - {self.check_in}"
